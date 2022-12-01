588a7138f8a7192c5acc8a748638784a975d12e9
Ivan <ivancordonm@gmail.com>
2022-12-01 06:36:22 +0100

    initial commit

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,46 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var partial = 0
+        for (weight in input) {
+            if(weight.isNotBlank()) {
+                partial += weight.toInt()
+            } else {
+                if (partial > max) max = partial
+                partial = 0
+            }
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var partial = 0
+        val weights = ArrayList<Int>()
+        for (weight in input) {
+            if(weight.isNotBlank() ) {
+                partial += weight.toInt()
+            } else {
+                weights.add(partial)
+                partial = 0
+            }
+        }
+        weights.add(partial)
+        val sortedList = weights.sortedDescending()
+        return sortedList[0] + sortedList[1] + sortedList[2]
     }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
+
+    val soltest1 = part1(testInput)
+    check(soltest1 == 24000)
     println(part1(input))
+
+    val soltest2 = part2(testInput)
+    check(soltest2 == 45000)
     println(part2(input))
+
+
 }
