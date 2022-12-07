a41c11fd17eb01a2a7e86bed21f9748be65db3ed
Ivan <ivancordonm@gmail.com>
2022-12-07 14:15:57 +0100

    day 7: fixed

--- src/Day07.kt
+++ src/Day07.kt
@@ -2,6 +2,8 @@ import java.util.*
 
 fun main() {
 
+    fun List<String>.path() = fold("") { acc, elem -> "$acc/$elem" }
+
     fun dirSizes(commands: List<String>): Map<String, Int> {
 
         val sizes = mutableMapOf<String, Int>()
@@ -14,46 +16,51 @@ fun main() {
                     if (splitCommand[2] == "..") stack.pop()
                     else {
                         stack.push(splitCommand[2])
-                        sizes[splitCommand[2]] = 0
+                        val idx = stack.toList().path()
+                        sizes[idx] = 0
                     }
                 }
+
                 "ls" -> continue
                 else -> {
                     if (splitCommand[0] != "dir") {
                         val sz = splitCommand[0].toInt()
-                        for (dir in stack) sizes[dir] = sizes[dir]!! + sz
+                        for (i in 1..stack.size) {
+                            val idx = stack.take(i).path()
+                            sizes[idx] = sizes[idx]!! + sz
+                        }
                     }
                 }
             }
         }
 
-//        println(sizes)
         return sizes.toMap()
     }
 
 
     fun part1(input: List<String>): Int {
-        println(dirSizes(input).filter { it.value <= 100000 })
 
         return dirSizes(input).filter { it.value <= 100000 }.map { it.value }.sum()
     }
 
 
+
     fun part2(input: List<String>): Int {
 
-        return 0
-    }
+        val sizes = dirSizes(input)
+        val rescue = 30000000 - 70000000 + sizes["//"]!!
 
+        return (sizes.toList().filter { it.second >= rescue }.minByOrNull { it.second }!!.second)
+    }
 
     val testInput = readInput("Day07_test")
     val input = readInput("Day07")
 
-
     println(part1(testInput))
-//    check(part1(testInput) == 95437)
+    check(part1(testInput) == 95437)
     println(part1(input))
 
-//    println(part2(testInput))
-//    check(part2(testInput) == 95437)
-//    println(part2(input))
+    println(part2(testInput))
+    check(part2(testInput) == 24933642)
+    println(part2(input))
 }
