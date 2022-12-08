eb9e5a3cd52b1076a29c6ecb85af7bef7772df97
Ivan <ivancordonm@gmail.com>
2022-12-08 09:04:59 +0100

    day 8: done

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,40 +1,60 @@
 fun main() {
 
-
     fun part1(input: List<String>): Int {
 
-        var total = 0
-        val top = 0
-        val left = 0
-        val down = input.size - 1
-        val right = input.first().length
+        val matrix = input.map { it.map { elem -> elem.digitToInt() } }
+        val transpose = List(matrix.first().size) { index -> matrix.map { it[index] } }
 
-        for ((i, row) in input.withIndex())
-            for ((j, elem) in row.withIndex()) {
-                if (i == top || i == down || j == left || j == right) {
+        var total = input.size * 4 - 4
+
+        for (i in 1..input.size - 2)
+            for (j in 1..input.size - 2)
+                if (transpose[j].take(i).max() < matrix[i][j] ||
+                    transpose[j].takeLast(input.size - i - 1).max() < matrix[i][j] ||
+                    matrix[i].take(j).max() < matrix[i][j] ||
+                    matrix[i].takeLast(input.size - j - 1).max() < matrix[i][j])
+
                     total++
-                    continue
-                }
-                if (input[i - 1][j] <= elem || input[i][j - 1] <= elem || input[i + 1][j] <= elem || input[i][j + 1] <= elem) total++
-            }
-
         return total
     }
 
 
     fun part2(input: List<String>): Int {
 
-        TODO()
+        fun countTreesLower(current: Int, trees: List<Int>): Int {
+            var count = 0
+            for(tree in trees) {
+                count++
+                if(current <= tree) break
+            }
+            return count
+        }
+
+        val matrix = input.map { it.map { elem -> elem.digitToInt() } }
+        val transpose = List(matrix.first().size) { index -> matrix.map { it[index] } }
+
+        var maxTrees = 0
+
+        for (i in 1..input.size - 2)
+            for (j in 1..input.size - 2) {
+                var partialMaxTrees  = countTreesLower(matrix[i][j],transpose[j].take(i).reversed())
+                partialMaxTrees *=  countTreesLower(matrix[i][j],transpose[j].takeLast(input.size - i - 1))
+                partialMaxTrees *=  countTreesLower(matrix[i][j],matrix[i].take(j).reversed())
+                partialMaxTrees *=  countTreesLower(matrix[i][j],matrix[i].takeLast(input.size - j - 1))
+                if(partialMaxTrees > maxTrees) maxTrees = partialMaxTrees
+            }
+        return maxTrees
     }
 
     val testInput = readInput("Day08_test")
     val input = readInput("Day08")
 
+//    part1(testInput)
     println(part1(testInput))
     check(part1(testInput) == 21)
     println(part1(input))
 
-//    println(part2(testInput))
-//    check(part2(testInput) == 0)
-//    println(part2(input))
+    println(part2(testInput))
+    check(part2(testInput) == 8)
+    println(part2(input))
 }
