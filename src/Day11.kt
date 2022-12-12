acdde8056dec27b9d99896d0128b27217371728e
Ivan <ivancordonm@gmail.com>
2022-12-12 18:02:47 +0100

    day 11: finished

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,26 +1,25 @@
 fun main() {
 
-    class Monkey(
+    data class Monkey(
         var items: MutableList<Long>,
         val operation: String,
         val opValue: String,
         val div: Long,
         val ifTrue: Int,
         val ifFalse: Int,
-        var intemsRevised: Long
+        var itemsRevised: Long
     )
 
-    fun getMonkeysInfo(input: List<String>): List<Monkey> {
-        val monkeys = mutableListOf<Monkey>()
+    fun buildMonkeys(input: List<String>): List<Monkey> = buildList {
         for (info in input) {
             val monkeyInfo = info.split("\n").drop(1)
             val items = "(\\d+),?".toRegex().findAll(monkeyInfo[0]).map { it.groupValues[1].toLong() }.toList()
-            val operation = "(\\*|\\+) (\\d+|old)".toRegex().find(monkeyInfo[1])!!.groupValues.toList()
+            val operation = "([*+]) (\\d+|old)".toRegex().find(monkeyInfo[1])!!.groupValues.toList()
             val divisible = "(\\d+),?".toRegex().findAll(monkeyInfo[2]).map { it.value }.first().toLong()
             val trueToMonkey = "(\\d+)".toRegex().findAll(monkeyInfo[3]).map { it.value }.first().toInt()
             val falseToMonkey = "(\\d+)".toRegex().findAll(monkeyInfo[4]).map { it.value }.first().toInt()
 
-            monkeys.add(
+            add(
                 Monkey(
                     mutableListOf(*items.toTypedArray()),
                     operation[1],
@@ -31,73 +30,46 @@ fun main() {
                     0
                 )
             )
-
         }
-        return monkeys.toList()
     }
 
-    fun part1(input: List<String>): Long {
-
-        val monkeys = getMonkeysInfo(input)
-        repeat(20) {
+    fun countItems(monkeys: List<Monkey>, iterations: Int, reduce: Int): List<Long> {
+        val base = monkeys.map { it.div }.reduce { acc, i -> acc * i }
+        repeat(iterations) {
             for (monkey in monkeys) {
                 for (item in monkey.items) {
-                    monkey.intemsRevised++
+                    monkey.itemsRevised++
                     val value = when (monkey.opValue) {
                         "old" -> item
                         else -> monkey.opValue.toLong()
                     }
                     val total = when (monkey.operation) {
-                        "*" -> (item * value) / 3
-                        "+" -> (item + value) / 3
+                        "*" -> (item * value) / reduce
+                        "+" -> (item + value) / reduce
                         else -> item
                     }
                     if (total % monkey.div == 0L) {
-//                        println("Monkey ${monkeys.indexOf(monkey)}: item $total -> ${monkey.ifFalse}")
-                        monkeys[monkey.ifTrue].items.add(total)
+                        monkeys[monkey.ifTrue].items.add(total % base)
                     } else {
-//                        println("Monkey ${monkeys.indexOf(monkey)}: item $total -> ${monkey.ifFalse}")
-                        monkeys[monkey.ifFalse].items.add(total)
+                        monkeys[monkey.ifFalse].items.add(total % base)
                     }
                 }
                 monkey.items = mutableListOf()
             }
-//            monkeys.forEach { println(it.items) }
         }
-//        println(monkeys.map { it.intemsRevised })
-        return monkeys.map { it.intemsRevised }.sortedDescending().let { it[0] * it[1] }
+
+        return monkeys.map { it.itemsRevised }
+
     }
 
-    fun part2(input: List<String>): Long {
-        val monkeys = getMonkeysInfo(input)
-        val common = monkeys.map { it.div }.reduce { acc, elem -> acc * elem }
-        repeat(10000) {
-            for (monkey in monkeys) {
-                for (item in monkey.items) {
-                    monkey.intemsRevised++
-                    val value = when (monkey.opValue) {
-                        "old" -> item
-                        else -> monkey.opValue.toLong()
-                    }
-                    val total = when (monkey.operation) {
-                        "*" -> item * value
-                        "+" -> item + value
-                        else -> item
-                    }
-                    if (total % monkey.div == 0L) {
-//                        println("Monkey ${monkeys.indexOf(monkey)}: item $total -> ${monkey.ifFalse}")
-                        monkeys[monkey.ifTrue].items.add(total % common)
-                    } else {
-//                        println("Monkey ${monkeys.indexOf(monkey)}: item $total -> ${monkey.ifFalse}")
-                        monkeys[monkey.ifFalse].items.add(total % common)
-                    }
-                }
-                monkey.items = mutableListOf()
-            }
-//            monkeys.forEach { println(it.items) }
-        }
-//        println(monkeys.map { it.intemsRevised })
-        return monkeys.map { it.intemsRevised }.sortedDescending().let { it[0] * it[1] }
+    fun part1(monkeys: List<Monkey>): Long {
+        val counter = countItems(monkeys, 20, 3)
+        return counter.sortedDescending().take(2).reduce{acc, elem -> acc * elem }
+    }
+
+    fun part2(monkeys: List<Monkey>): Long {
+        val counter = countItems(monkeys, 10000, 1)
+        return counter.sortedDescending().take(2).reduce{acc, elem -> acc * elem }
     }
 
 
@@ -105,12 +77,11 @@ fun main() {
     val input = readInputByGroups("Day11")
 
 
-    check(part1(testInput) == 10605L)
-    println(part1(testInput))
-    println(part1(input))
+    check(part1(buildMonkeys(testInput)) == 10605L)
+    println(part1(buildMonkeys(input)))
 
-    println(part2(testInput))
-    check(part2(testInput) == 2713310158)
-    println(part2(input))
+
+    check(part2(buildMonkeys(testInput)) == 2713310158L)
+    println(part2(buildMonkeys(input)))
 
 }
